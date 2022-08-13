0931d12322f19022addba131735010ea6c76907f
Victor Lemos <victorhbl12@gmail.com>
2022-08-13 00:19:27 -0300

    [Changelog]
    Add - new custom github widget for the projects section.

---
+++ assets/js/github-repos-widget.js
@@ -0,0 +1,39 @@
+// Get projetcs from the Github API to display in the projects section
+const PROJECTS_TO_DISPLAY = ['Classification-Score', 'MaxCliqueGrasp', 'RooftopSegmentation', 'PI-Projects', 'Power-Load-Predictor']
+const MAX_DESCRIPTION_LENGTH = 43
+const GITHUB_API_ENDPOINT = "https://api.github.com/users/vituenrique/repos"
+
+$.ajax({
+    url: GITHUB_API_ENDPOINT,
+    type: 'GET',
+    dataType: 'json', 
+    success: function(repositories) {
+        repositories = repositories.filter((repo) => PROJECTS_TO_DISPLAY.includes(repo.name))
+        console.log(repositories)
+        repositories.forEach(repo => {
+            let repository_url = repo.html_url
+            let repository_full_name = repo.full_name
+            let repository_description = (repo.description ?? "") 
+            repository_description = repository_description.length > MAX_DESCRIPTION_LENGTH ? repository_description.substring(0, MAX_DESCRIPTION_LENGTH) + " ..." : repository_description
+            let repository_programing_language = repo.language
+            let repository_programing_language_img = "../assets/img/logo/programing_languages/" + repository_programing_language + ".svg"
+            let repository_cover_img = "../assets/img/banner_github.png"
+            $('#github-projects').append(
+                "<div class='grid-item wow zoomIn'>\
+                    <div class='img-place'>\
+                        <a href='" + repository_url + "' target='_brank'>\
+                            <img src='" + repository_cover_img + "'alt=''>\
+                            <div style='height: 65px'></div>\
+                            <div class='img-caption'>\
+                                <h5 class='fg-theme' style='font-size: 18px;'>" + repository_full_name + "</h5>\
+                                <p style='display:" + (repository_description.length > 0 ? "block" : "none") + ";'>" + repository_description + "</p>\
+                                <p style='bottom: 16px;position: absolute;'><img style='width: 5%;' src='" + repository_programing_language_img + "'alt=''> "  + repository_programing_language + "</p>\
+                            </div>\
+                        </a>\
+                    </div>\
+                </div>"
+            );
+        }); 
+    }
+});
+
---
+++ assets/js/multi-language-switch.js
@@ -0,0 +1,9 @@
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
