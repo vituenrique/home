798434c538fd9ff1a950ae8587bc69f312146cf1
Victor Lemos <victorhbl12@gmail.com>
2022-08-20 22:06:05 -0300

    [Changelog]
    Add - experiences widget
    Fix - button name in the papers section

---
+++ assets/js/experiences-widget.js
@@ -0,0 +1,26 @@
+import experiences_json from './experiences.json' assert {type: 'json'};
+
+const EXPERIENCES = ['/', '/home/'].includes(window.location.pathname) ? experiences_json['experiences']['en-US'] : experiences_json['experiences']['pt-BR']
+const NOT_AVAILABLE_ERROR =  ['/', '/home/'].includes(window.location.pathname) ? "Not available right now..." : "Indisponível no momento...." 
+
+function displayExperiences(experience) {
+    let new_row = "<li>\
+                    <div class='title'>" + experience.period + "</div>\
+                    <div class='details'>\
+                        <h5>" + experience.title + "</h5>\
+                        <small class='fg-theme'>" + experience.company + "</small>\
+                        <p>" + experience.job_description + "</p>\
+                    </div>\
+                <li>";
+
+    $('#experiences-widget').append(new_row);
+}
+
+$(document).ready( () => {
+    if (EXPERIENCES.length > 0) {
+        EXPERIENCES.forEach((paper) => displayExperiences(paper));
+    } else {
+        $('#experiences-widget').css("display", "none")
+        $('#experiences-widget').append("<h3 class='text-center wow fadeInUp'>" + NOT_AVAILABLE_ERROR + "</h3>")
+    }
+});
--- assets/js/papers-widget.js
+++ assets/js/papers-widget.js
@@ -1,18 +1,18 @@
 import papers_json from './papers.json' assert {type: 'json'};
 
-const papers = papers_json['papers']
+const PAPERS = papers_json['papers']
 const NOT_AVAILABLE_ERROR =  ['/', '/home/'].includes(window.location.pathname) ? "Not available right now..." : "Indisponível no momento...." 
+const VIEW_BTN_LABEL =  ['/', '/home/'].includes(window.location.pathname) ? "View" : "Ver" 
 
-
-function displayPaper(paper, index) {
+function displayPaper(paper) {
     let new_row = "<tr>\
                     <td>" + paper.title + "</td>\
                     <td>" + paper.publisher + "</td>\
                     <td>" + paper.year + "</td>\
                     <td>\
                         <a href='" + paper.link + "' target='_blank' class='btn btn-secondary btn-sm " + (paper.link == "#" ? "disabled" : "") + "' tabindex='-1' role='button' aria-disabled='true'>\
-                            <span class='icon ti-link'></span> View\
-                        </a>\
+                            <span class='icon ti-link'></span>" + VIEW_BTN_LABEL
+                        "</a>\
                     </td>\
                 <tr>";
 
@@ -20,8 +20,8 @@ function displayPaper(paper, index) {
 }
 
 $(document).ready( () => {
-    if (papers.length > 0) {
-        papers.forEach((paper, index) => displayPaper(paper, index));
+    if (PAPERS.length > 0) {
+        PAPERS.forEach((paper) => displayPaper(paper));
     } else {
         $('#papers-table').css("display", "none")
         $('#papers-error').append("<h3 class='text-center wow fadeInUp'>" + NOT_AVAILABLE_ERROR + "</h3>")
